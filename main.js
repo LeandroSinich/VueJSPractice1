4d55841f00f3f614756cb63e071009b7ca41b016
LeandroSinich <leandrosinich@gmail.com>
2024-08-16 12:17:05 -0300

    clase 5 y 6, v-for, v-on:click, methods

--- main.js
+++ main.js
@@ -5,7 +5,24 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inventory: 11,
             url: 'https://www.linkedin.com/in/leandrosinich/',
-            onSale: true
+            details: ['50% cotton', '30% wool','20% polyester'],
+            variants:[
+                {id:2234, color: 'green', image:'./assets/images/socks_green.jpg'},
+                {id:2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            ],
+            cart: 0
+            
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        deleteFromCart(){
+            this.cart -= 1
         }
     }
 })
