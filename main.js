34b1b9219be6f94f512c2556754f34d053dff2df
LeandroSinich <leandrosinich@gmail.com>
2024-08-19 11:16:40 -0300

    computed properties

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const app = Vue.createApp({
     data(){
         return{
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 11,
+            product: 'Socks',            
+            selectedVariant: 0,
             url: 'https://www.linkedin.com/in/leandrosinich/',
             details: ['50% cotton', '30% wool','20% polyester'],
             variants:[
-                {id:2234, color: 'green', image:'./assets/images/socks_green.jpg'},
-                {id:2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+                {id:2234, color: 'green', image:'./assets/images/socks_green.jpg', quantity: 10},
+                {id:2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
             ],
-            cart: 0
+            cart: 0,
+            brand: 'Vue Mastery',
+            onSale: false
             
         }
     },
@@ -18,11 +19,26 @@ const app = Vue.createApp({
         addToCart(){
             this.cart += 1
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateVariant(index){
+            this.selectedVariant = index
+            console.log(index)
         },
         deleteFromCart(){
             this.cart -= 1
         }
+    },
+    computed: {
+        title(){
+            return this.brand + '' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },    
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        isOnSale(){
+            return `${this.brand} ${this.product} is on sale`
+        }    
     }
 })
